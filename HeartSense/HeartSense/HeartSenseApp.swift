6eeaa01ca6c7503efb71bc32c593ad0c51ea9092
Aditya Naravane <adityanaravane2008@gmail.com>
2025-10-22 18:16:42 -0400

    added swift to heartsense folder

---
+++ HeartSense/HeartSense/HeartSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartSenseApp.swift
+//  HeartSense
+//
+//  Created by Aditya Naravane on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct HeartSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
